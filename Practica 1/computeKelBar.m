657aee4f8ec6456de9879bc7f6706ca1b154b46f
Guiiem <43451847+Guiiem@users.noreply.github.com>
2018-09-28 15:08:36 +0200

    Passat a 3D de manera q funcioni (abans no funcionava)

--- Practica 1/computeKelBar.m
+++ Practica 1/computeKelBar.m
@@ -1,4 +1,4 @@
-function [Kel,massa_bc] = computeKelBar(Ndim,Nelements,x,Tn,mat,Tmat)
+function Kel = computeKelBar(Ndim,Nelements,x,Tn,mat,Tmat)
 %--------------------------------------------------------------------------
 % The function takes as inputs:
 %   - Dimensions:  Ndim        Problem's dimensions
@@ -17,32 +17,61 @@ function [Kel,massa_bc] = computeKelBar(Ndim,Nelements,x,Tn,mat,Tmat)
 %   - Kel   Elemental stiffness matrices [NdofsXelement x NdofsXelement x Nelements]
 %            Kel(i,j,e) - Term in (i,j) position of stiffness matrix for element e
 %--------------------------------------------------------------------------
-Kel = zeros(2*Ndim, 2*Ndim, Nelements);
-massa_bc = 0;
 
-for e = 1:Nelements
-    x_1 = x(Tn(e,1),1);
-    x_2 = x(Tn(e,2),1);
-    y_1 = x(Tn(e,1),2); 
-    y_2 = x(Tn(e,2),2);
-    %%if Ndim == 3 %Per pode canviar-ho com volguem.
-    z_1 = x(Tn(e,1),3);
-    z_2 = x(Tn(e,2),3);
-    %%else
-    
-    l = sqrt((x_2-x_1)^2+(y_2-y_1)^2+(z_2-z_1)^2);
-    R_2= [x_2-x_1 y_2-y_1 z_2-z_1 0 0 0; 
-        0 0 0 x_2-x_1 y_2-y_1 z_2-z_1];
-    R = (1/l)*R_2; %2x6 matrix
-    Kt = ((mat(Tmat(e),2)*mat(Tmat(e),1))/l)*[1, -1;-1, 1]; %2x2
-    K = transpose(R)*Kt*R; %6x2 * 2x2 * 2x6; 
-    massa_bc = massa_bc + mat(Tmat(e),2)*mat(Tmat(e),3)*l;
-    for i = 1:4
-        for j = 1:4
-            Kel(i,j,e) = K(i,j);
+% Longituds de barra
+j=0;
+for j=1:Nelements
+    if(Ndim==2) % 2D
+   L(j,1)=[sqrt((x(Tn(j,1),1)-(x(Tn(j,2),1)))^2 + ...
+       + (x(Tn(j,1),2)-(x(Tn(j,2),2)))^2)];
+    else % 3D
+    L(j,1)=[sqrt((x(Tn(j,1),1)-(x(Tn(j,2),1))^2 + ...
+       + (x(Tn(j,1),2)-(x(Tn(j,2),2)))^2 + ...
+       + (x(Tn(j,1),3)-(x(Tn(j,2),3)))^2))];
+    end
+end    
+
+% Matriu R per cada element
+j=0; 
+    for j=1:Nelements
+        if(Ndim==2) % 2D
+            % fila x col x profunditat 
+R(1,1,j)= 1/L(j)*( (x(Tn(j,2),1)-x(Tn(j,1),1))); R(2,3,j)=1/L(j)*( (x(Tn(j,2),1)-x(Tn(j,1),1)));    
+R(1,2,j)= 1/L(j)*(x(Tn(j,2),2)-x(Tn(j,1),2)); R(2,4,j)= 1/L(j)*(x(Tn(j,2),2)-x(Tn(j,1),2));
+R(1,3)=0; R(1,4)=0; R(2,1)=0; R(2,2)=0;
+
+        else % 3D
+R(1,1,j)= 1/L(j)*( (x(Tn(j,2),1)-x(Tn(j,1),1))); 
+R(1,2,j)= 1/L(j)*(x(Tn(j,2),2)-x(Tn(j,1),2));
+R(1,3,j)= 1/L(j)*(x(Tn(j,2),3)-x(Tn(j,1),3));
+
+R(2,4,j)=1/L(j)*( (x(Tn(j,2),1)-x(Tn(j,1),1)));    
+R(2,5,j)= 1/L(j)*(x(Tn(j,2),2)-x(Tn(j,1),2));
+R(2,6,j)= 1/L(j)*(x(Tn(j,2),2)-x(Tn(j,1),2)); 
+
+R(1,4)=0; R(1,5)=0; R(1,6)=0; R(2,1)=0; R(2,2)=0; R(2,3)=0;
+
         end
     end
+    
+% K prima
+e=0;
+ for e=1:Nelements
+        % fila x col x profunditat
+   k=mat(Tmat(e),1)*mat(Tmat(e),2)/L(e,1)*1; %E*A/L
+        %if(Ndim==2) % 2D
+   Kprima(1,1,e) = k; Kprima(1,2,e) = -k;
+   Kprima(2,1,e) = -k; Kprima(2,2,e) = k; %Sempre es la mateixa matriu 2x2 WHYYY?
+        %else % 3D
+   %Kprima(1,1,e) = k; Kprima(1,2,e) = -k; Kprima(1,3,e) = -k;
+   %Kprima(2,1,e) = -k; Kprima(2,2,e) = k; Kprima(2,3,e) = -k;    
+   %Kprima(3,1,e) = -k; Kprima(3,2,e) = -k; Kprima(3,3,e) = k;         
+        %end
+ end
+ 
+j=0;
+for j=1:Nelements
+    Kel(:,:,j) = R(:,:,j).'*Kprima(:,:,j)*R(:,:,j);
 end
-
 
 end
